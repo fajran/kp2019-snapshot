f94e3ceae055dfebf41c535d463f35cbdac271c4
Fajran Iman Rusadi <frusadi@comscore.com>
2019-04-20 20:37:46 +0200

    add script

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+
+curl https://kawal-c1.appspot.com/api/c/0 > 0.json
+
+cat 0.json | jq '.children[][0]' > 0_children_ids.txt
+
+cat 0_children_ids.txt | while read cid
+do
+  curl https://kawal-c1.appspot.com/api/c/$cid > $cid.json
+done
+
+git add "*.json" "*.txt"
+git commit -m "$(date -R)"
+git push origin master
+
